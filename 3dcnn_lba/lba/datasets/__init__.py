c65887fbf451e15628fed4485ac07bafb94229f3
SaisaiSun <72762032+SaisaiSun@users.noreply.github.com>
2024-01-05 19:35:21 +0800

    Delete 3dcnn_lba directory

--- 3dcnn_lba/lba/datasets/__init__.py
+++
@@ -1,1 +0,0 @@
-from .datasets import LMDBDataset, PDBDataset, PTGDataset, SilentDataset, load_dataset, make_lmdb_dataset, get_file_list, extract_coordinates_as_numpy_arrays
--- 3dcnn_lba/lba/datasets/scripts/remove_empty.py
+++
@@ -1,9 +0,0 @@
-from lba.datasets import load_dataset, make_lmdb_dataset
-import sys
-
-in_path = sys.argv[1]
-out_path = sys.argv[2]
-
-dataset = load_dataset(in_path, 'lmdb')
-
-make_lmdb_dataset(dataset, out_path, filter_fn=lambda x: not x)
